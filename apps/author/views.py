539731fb7efed5c2ee8cf8a8bed4142839325c22
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2020-02-28 11:08:11 -0300

    Create viewset class for authors

--- apps/author/views.py
+++ apps/author/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AuthorSerializer
+from .models import Author
 
-# Create your views here.
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    http_method_names = ['get']
+
+    def get_queryset(self):
+        return Author.objects.all()
