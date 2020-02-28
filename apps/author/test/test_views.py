2278dd8017be1c6a760f525a488e94ec60266601
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2020-02-28 11:57:51 -0300

    Add tests for the Authors views

---
+++ apps/author/test/test_views.py
@@ -0,0 +1,41 @@
+from django.test import TestCase, Client
+from rest_framework import status
+from apps.author.models import Author
+from apps.author.serializers import AuthorSerializer
+
+client = Client()
+
+
+class AuthorViewTest(TestCase):
+    def setUp(self):
+        author_model = Author(
+            name="Luciano Ramalho"
+        )
+        author_model.save()
+
+        author_model = Author(
+            name="Osvaldo Santana Neto"
+        )
+        author_model.save()
+
+    def test_get_author(self):
+        author_id = 1
+        response = client.get(f'/api/v1/authors/{author_id}/')
+        author = Author.objects.get(pk=author_id)
+
+        self.assertEqual(author.id, response.data['id'])
+        self.assertEqual(author.name, response.data['name'])
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_get_all_authors(self):
+        response = client.get('/api/v1/authors/')
+
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+
+        self.assertCountEqual(response.data, serializer.data)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+    def test_get_author_404(self):
+        response = client.get('/api/v1/authors/404/')
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
