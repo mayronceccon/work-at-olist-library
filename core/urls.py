596bfcb9c549a8003da575dee7149c0b6c5b22e2
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2020-02-28 11:16:26 -0300

    Insert URL in the API to consult authors

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from apps.author.views import AuthorViewSet
 
 
 router = routers.DefaultRouter()
+router.register(r'authors', AuthorViewSet, basename='authors')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
