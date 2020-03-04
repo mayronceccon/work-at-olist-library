2dc9e90dac8a9421af3b8deb9db53df469e35386
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2020-03-04 14:24:13 -0300

    Add constante STATICFILES_DIRS

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
