4db8b2fedd1866a99181b904d8c9003f710505cf
Mayron Thomas Ceccon <mayron.ceccon@gmail.com>
2020-03-05 12:06:49 -0300

    Change initial URL of project

--- core/urls.py
+++ core/urls.py
@@ -25,16 +25,16 @@ router.register(r'authors', AuthorViewSet, basename='authors')
 router.register(r'books', BookViewSet, basename='books')
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
-    path('admin/', admin.site.urls),
     url(
-        r'^documentation(?P<format>\.json|\.yaml)$',
+        r'^(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0),
         name='schema-json'
     ),
     url(
-        r'^documentation/$',
+        r'^$',
         schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'
     ),
+    path('api/v1/', include(router.urls)),
+    path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
